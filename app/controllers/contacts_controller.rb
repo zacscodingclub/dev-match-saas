ea9a0262b397e322fde5541856bfc3c536d23807
Zac Baston <zbaston@gmail.com>
2015-09-28 22:53:43 +0000

    added Mailer and view

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -7,13 +7,17 @@ class ContactsController < ApplicationController
     @contact = Contact.new(contact_params)
     
     if @contact.save
+      name = params[:contact][:name]
+      email = params[:contact][:email]
+      body = params[:contact][:comments]
+      ContactMailer.contact_email(name, email, body).deliver
+      
       flash[:success] = 'Thanks for your message. An associate will respond as soon as possible.'
       redirect_to new_contact_path
     else
       flash[:danger] = 'An error occurred. Please try again.'
       redirect_to new_contact_path
     end
-  
   end
   
   private
