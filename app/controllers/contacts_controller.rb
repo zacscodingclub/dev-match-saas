90504722e2c640202d8cf56ce731882b573336ff
Zac Baston <zbaston@gmail.com>
2015-09-28 21:40:48 +0000

    added Contact form with some validation and save to database

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -4,6 +4,20 @@ class ContactsController < ApplicationController
   end
   
   def create
+    @contact = Contact.new(contact_params)
     
+    if @contact.save
+      flash[:success] = 'Thanks for your message. An associate will respond as soon as possible.'
+      redirect_to new_contact_path
+    else
+      flash[:danger] = 'An error occurred. Please try again.'
+      redirect_to new_contact_path
+    end
+  
   end
+  
+  private
+    def contact_params
+      params.require(:contact).permit(:name, :email, :comments)
+    end
 end
